8aaf05d41d136cad635a71cee965b3aa176b7e07
home <home@DESKTOP-J1NO4IN>
2021-03-31 17:48:36 +0900

    1111

--- TeamProject1/src/com/sist/model/tripModel.java
+++ TeamProject1/src/com/sist/model/tripModel.java
@@ -1,9 +1,24 @@
 package com.sist.model;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
 
+
 @Controller
 public class tripModel {
+  @RequestMapping("trip/trip_detail.do")
+  public String trip_detail(HttpServletRequest request,HttpServletResponse response)
+	{
+	 	String cno=request.getParameter("cno");
+	 	// DAO 연결
+	 	
+		request.setAttribute("main_jsp", "../trip/trip_detail.jsp"); 
+		return "../main/main.jsp"; 
+	}
   
 }
