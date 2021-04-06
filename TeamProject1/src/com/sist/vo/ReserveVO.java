e0cce4e494068ab77a0c89e144b525b7152d22a7
rrddk <rrddk@DESKTOP-GBETOB4>
2021-04-06 18:01:26 +0900

    11111

---
+++ TeamProject1/src/com/sist/vo/ReserveVO.java
@@ -0,0 +1,31 @@
+package com.sist.vo;
+/*
+ *  no NUMBER,
+  id VARCHAR2(20),
+  title VARCHAR(200),
+  day VARCHAR2(30),
+  time VARCHAR2(10),
+  inwon VARCHAR2(10),
+  state NUMBER
+ */
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+public class ReserveVO {
+   private int no;
+   private String id;
+   private String title;
+   private String inday;
+   private String outday;
+   private String intime;
+   private String outtime;
+   private String inwon;
+   private int room;
+   private int state;
+   private Date regdate;
+   private String dbday;
+}
