fb6698ab4717e56041c7f39cd72f368050613074
rrddk <rrddk@DESKTOP-GBETOB4>
2021-04-09 10:52:26 +0900

    1111

---
+++ TeamProject1/src/com/sist/vo/HotelImageVO.java
@@ -0,0 +1,14 @@
+package com.sist.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HotelImageVO {
+	private int no;
+	private int cno;
+	private String poster;
+	private String title;
+	private String category;
+}
